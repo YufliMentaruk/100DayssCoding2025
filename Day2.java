9d175e7c61f14f23e45645828d902a6d83dcfedc
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-09 15:00:05 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,10 @@
+public class Main {
+public static void main(String[] args) {
+        System.out. print("Nama saya yufli ");
+
+            System.out.println("Nama saya yufli ");
+
+             System.out.printf("Nama saya %s berumur %d", "Yufli", 21);
+        
+    }
+}
