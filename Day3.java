42fcd922b2d3850efea11523471c13423336b2d8
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-10 14:09:41 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,9 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("Contoh Komentar1");
+        //ini untuk single line
+        System.out.println("Contoh Komentar2");
+        /*ini untuk multiline
+         */
+    }
+}
