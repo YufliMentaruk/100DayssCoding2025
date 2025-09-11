6e311a708c42bbf2268ccd1228e06ceebb8035b8
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-11 21:34:22 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,19 @@
+public class Day4 {
+    public static void main(String[] args) {
+                System.out.println("Halo\nSelamat Datang Di");
+                // \n ini untuk baris baru
+                System.out.println("Nama:\tYufli");
+                System.out.println("NIM:\tD0224508");
+                // \t ini untuk tab
+                System.out.println("Dia berkata: \"Belajar Merangkai itu asik!\"");
+                // \" ini untuk tanda kutip ganda
+                System.out.println("Karakter tunggal: \'A\'");
+                // \' ini untuk tanda kutip tunggal
+                System.out.println("Lokasi file: C:\\Users\\Yufli\\Documents");
+                // \\ ini untuk backslash
+                System.out.println("Halo Dunia\rElektronika");
+                // \r ini untukcarriage return (teks setelah \r menimpa dari awal baris)
+                System.out.println("Belajarr\b Merangkai");
+                // \b ini untuk backspace (menghapus 1 karakter sebelumnya)
+            }
+        }
