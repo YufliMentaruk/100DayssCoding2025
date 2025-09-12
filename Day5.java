56c4c9108d6835e85953cdc98ea2fb876339c809
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-12 21:16:17 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class Day5 {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        byte A = 120;
+        short B = 30000;
+        int C = 1000000;
+        long D = 10000000000L; // tambahkan L karena nilainya besar
+
+        // Menampilkan isi variabel
+        System.out.println("Contoh Tipe Data Bilangan Bulat:");
+        System.out.println("Byte  : " + A);
+        System.out.println("Short : " + B);
+        System.out.println("Int   : " + C);
+        System.out.println("Long  : " + D);
+    }
+}
