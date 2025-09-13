4ef94bea18cf618d9495dbe3c9b4c3ceaf50cc47
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-13 20:04:26 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,19 @@
+public class Day6 {
+    public static void main(String[] args) {
+        // Tipe data float
+        float tinggiBadan = 164.5f;   // perlu huruf f di akhir
+        float suhu = 30.6f;
+        // Tipe data double
+        double beratBadan = 55.223558780;  // lebih presisi
+        double pi = 3.141592653589793;
+
+        // Menampilkan nilai
+        System.out.println("Contoh Tipe Data Pecahan:");
+        // Tampilan float
+        System.out.println("Tinggi Badan    : " + tinggiBadan);
+        System.out.println("Suhu Tubuh      : " + suhu);
+        // Tampilan double
+        System.out.println("Berat Badan     : " + beratBadan);
+        System.out.println("Nilai Pi        : " + pi);
+    }
+}
