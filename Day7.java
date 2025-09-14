53bcedc65aece26791b576f54cc6eed26acd4a39
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-14 20:31:44 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+public class Day7 {
+    public static void main(String[] args) {
+      // Tipe data char
+      char inisialNama = 'Y';
+      char simbol ='*'
+      
+      // Tipe data boolean
+        boolean B= true;
+        boolean S= false;
+
+      // Tampilkan informasi
+        System.out.println("Inisial Nama      :" + inisialNama);
+        System.out.println("Simbol khusus     :" + simbol);
+        System.out.println("Yang Diitampilkan :" + B);
+        System.out.println("Yang Diitampilkan :" + S);
+    }
+}
