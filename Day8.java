a9f5db0df881f9b9a84c21d596ec1ce518b4e512
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-15 22:10:26 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,15 @@
+public class Day8 {
+    public static void main(String[] args) {
+      
+        // Deklarasi String
+        String nama  = "Yufli Mentaruk";
+        String nim   = "D0224508";
+        String kelas = "Informatika B";
+
+        // Untuk Menampilkan isi variabel String
+        System.out.println("Nama   : " + nama);
+        System.out.println("NIM    : " + nim);
+        System.out.println("Kelas  : " + kelas);
+
+    }
+}
