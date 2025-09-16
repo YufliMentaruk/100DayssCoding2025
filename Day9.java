a17d88e477cd6b0e5505ef043cb9d50e647cc913
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-16 19:57:02 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+public class Day9 {
+    public static void main(String[] args) {
+
+                // Deklarasi variabel
+                int umur = 18;
+                System.out.println("Umur awal: " + umur);
+                // Update variabel (ubah nilainya)
+                umur = 20;
+                System.out.println("Umur setelah di-update: " + umur);
+    }
+}
