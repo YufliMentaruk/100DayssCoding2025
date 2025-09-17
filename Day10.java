8bc90a57230b4c022b235ae873b0bb622263180b
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-17 21:08:09 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // konstanta PI
+        final double PI = 3.14;
+        // konstanta jumlah maksimal siswa
+        final int MAX_SISWA = 40;
+        
+        // PI = 3.15; // ❌ ERROR! Tidak boleh mengubah nilai konstanta
+        
+        System.out.println("Nilai PI: " + PI);
+        System.out.println("Jumlah maksimal siswa: " + MAX_SISWA);
+    }
+}
