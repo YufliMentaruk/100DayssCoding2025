02d9c2b5f95102851c09cb204d0a586fd44e1bae
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-18 22:41:08 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Day11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         // Input teks
         System.out.print("Masukkan nama: ");
         String nama = input.nextLine();
