49c6cc1dafc98649b18b2bbbc31e57c3ea26bdd3
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-21 21:06:25 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,10 @@
+public class Day14 {
+    public static void main(String[] args) {
+        // Tipe String awal
+        String strBoolean = "true";
+        // Konversi String ke tipe primitif
+        boolean status = Boolean.parseBoolean(strBoolean);
+        // Mencetak hasil konversi
+        System.out.println("String ke boolean: " + status);
+    }
+}
