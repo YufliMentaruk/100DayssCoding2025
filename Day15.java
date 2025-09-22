0933bf92492753cd901781ebcb4de7fe8f25edc6
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-22 20:26:47 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+public class Day15 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 14;
+        int b = 19;
+
+        // Operasi penjumlahan
+        int hasilTambah = a + b;
+        // Operasi pengurangan
+        int hasilKurang = a - b;
+
+        // Cetak hasil
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil penjumlahan dari a + b = " + hasilTambah);
+        System.out.println("Hasil pengurangan dari a - b = " + hasilKurang);
+    }
+}
