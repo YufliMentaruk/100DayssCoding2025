09e6bea07f1ae67993d57cdbc13d69073538bd5b
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-23 20:59:31 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,18 @@
+public class Day16 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 10;
+        int b = 5;
+
+        // Operasi perkalian
+        int Kali = a * b;
+        // Operasi pembagian
+        int Bagi = a / b;
+
+        // Cetak hasil
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil perkalian dari a * b = " + Kali);
+        System.out.println("Hasil pembagian dari a / b = " + Bagi);
+    }
+}
