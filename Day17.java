14bf95927b2623b7d6a53de65a521fe5ac1a0d99
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-24 20:25:42 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,8 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int a=14;
+        int b=9;
+        int sisaBagi= a % b;
+        System.out.println("Hasil SisaBagi dari a % b = " + sisaBagi);
+    }
+}
