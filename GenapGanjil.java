e8f211ab6d7ebd716a72af7018747c6e9171c174
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-25 23:24:37 +0800

    GenapGanjil.java

---
+++ GenapGanjil.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class D0224508 {   
+    public static void main(String[] args) {      
+       Scanner ip =new Scanner(System.in);
+            System.out.print("Masukkan Nilai :");
+            int b =ip.nextInt();
+
+            if(b%2==0){
+                System.out.println("Genap");
+            }else{
+                System.out.println("Ganjil");
+            }
+      }
+}
