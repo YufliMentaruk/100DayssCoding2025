b8ebd6a6b770a5027155802b5a77af1b95c71619
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-27 22:16:41 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+public class Day20 {
+    public static void main(String[] args) {
+        //Tipe data primitif
+        int a = 19;
+
+        //Dikonversi Ke nonprimif
+        String angka= String.valueOf(a);
+
+        System.out.println(angka);
+    }
+}
