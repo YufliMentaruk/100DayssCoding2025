09c3ba27757220709894483a18f3ceaf35cca365
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-29 21:44:03 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,8 @@
+public class Day22 {
+    public static void main(String[] args) {
+        int y=4;
+        int luas=y*y;
+        System.out.println("Sisi Persegi :"+ y);
+        System.out.println("Luas Persegi :"+ luas);
+    }
+}
