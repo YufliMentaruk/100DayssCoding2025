5e9cb53b98bc8aee0b02d2da3df8202acdf28b46
YufliMentaruk <yuflimentaruk@gmail.com>
2025-09-30 21:15:15 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,8 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int panjang =9;
+        int lebar   =4;
+        int luas = panjang*lebar;
+        System.out.println("Luas persegi :"+luas);
+    }
+}
