68ed54cd9d19a7b84d0136b052a392b54287105c
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-01 18:37:46 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,9 @@
+public class Day24 {
+    public static void main(String[] args) {
+        double l=9;
+        // Rumus luas lingkaran = PI * l * l
+        double luas = 3.14 * l * l;
+        // Mencetak hasil Rumus
+        System.out.println("Luas lingkaran\t:"+ luas);
+    }
+}
