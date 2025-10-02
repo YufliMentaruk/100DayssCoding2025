0174beea2f28188d6c7826dbfebe78cf28aefdb7
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-02 19:39:32 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,15 @@
+public class Day25 {
+    public static void main(String[] args) {
+        // Operator increment
+        int y = 14;
+        System.out.println("Nilai awal y\t\t= " + y);
+        y++;
+        System.out.println("Setelah increment\t= " + y);
+
+        // Operator decrement
+        int g =19;
+        System.out.println("Nilai awal g\t\t= "+g);
+        g--;
+        System.out.println("Setelah decrement\t= " + g);
+    }
+}
