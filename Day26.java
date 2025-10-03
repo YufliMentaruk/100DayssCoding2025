77a7be69f222587a6fa05da96904324246c4ed22
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-03 21:09:51 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,24 @@
+public class Day26 {
+    public static void main(String[] args) {
+        //Contoh operator penugasan
+        int a;
+        a =5;
+        int b=5;
+        b +=2;
+        int c=5;
+        c -=2;
+        int d=5;
+        d *=2;
+        double e=5;
+        e /=2;
+        int f=5;
+        f %=2;
+        //Menampilkan nilai
+        System.out.println("a ="+a);
+        System.out.println("b ="+b);
+        System.out.println("c ="+c);
+        System.out.println("d ="+d);
+        System.out.println("e ="+e);
+        System.out.println("f ="+f);
+    }
+}
