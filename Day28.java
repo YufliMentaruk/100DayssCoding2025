22d542f69f55544c4c14ea8dfd25b14ebd6b92c9
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-05 21:29:32 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,10 @@
+public class Day28 {
+    public static void main(String[] args) {
+        int a =14;
+        int b =19;
+        boolean c =(a==b);
+        boolean d =(a!=b);
+        System.out.println("Apakah a==b ?"+c);
+        System.out.println("Apakah a!=b ?"+d);
+    }
+}
