23495ea29c4141a0c92bff9c3079c6c1e79ae2ae
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-07 21:53:15 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,10 @@
+public class Day30 {
+    public static void main(String[] args) {
+        int g = 14;
+        int y = 19;
+        boolean hasil1 = (g <= y);
+        boolean hasil2 = (g >= y);
+        System.out.println("Apakah g <= y ? " + hasil1);
+        System.out.println("Apakah g >= y ? " + hasil2);
+    }
+}
