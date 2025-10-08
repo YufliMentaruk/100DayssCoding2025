089dddb7dae4ee90d7032d2188d3118c8b8341e0
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-08 19:22:53 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,9 @@
+public class Day31 {
+    public static void main(String[] args) {
+        int x = 10;
+        boolean hasil = (x > 5) && (x < 15); 
+
+        System.out.println("Nilai x: " + x);
+        System.out.println("Apakah x di antara 5 dan 15? " + hasil);
+    }
+}
