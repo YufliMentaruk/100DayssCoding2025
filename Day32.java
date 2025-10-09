d3e087de74618d489d8066f7dab132433d1fe1f6
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-09 23:09:07 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,9 @@
+public class Day32 {
+    public static void main(String[] args) {
+        int y = 14;
+        int g = 19;
+
+        boolean hasil = (y < 20) || (g > 25);
+        System.out.println("Apakah y < 20 atau g > 25 ? " + hasil);
+    }
+}
