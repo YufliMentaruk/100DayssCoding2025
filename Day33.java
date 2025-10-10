aaf29a6500f5259f333858cb9df4ea14cd799fe9
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-10 17:24:28 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class Day33 {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+        
+        System.out.println("Kebalikan dari true adalah : " + !benar);
+        System.out.println("Kebalikan dari false adalah : " + !salah);
+    }
+}
