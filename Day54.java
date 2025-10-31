0c017ab8f8e342da3259cf6f2a5774783619f19c
YufliMentaruk <yuflimentaruk@gmail.com>
2025-10-31 21:01:08 +0800

    Day54.java

---
+++ Day54.java
@@ -0,0 +1,10 @@
+public class Day54 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i == 10) {
+                continue;
+            }
+            System.out.println("Angka ke-" + i);
+        }
+    }
+}
