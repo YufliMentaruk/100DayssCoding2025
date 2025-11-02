9f1a8645f9da1f30e8db78f932e58874058d2a44
YufliMentaruk <yuflimentaruk@gmail.com>
2025-11-02 20:40:02 +0800

    Day56.java

---
+++ Day56.java
@@ -0,0 +1,9 @@
+public class Day56 {
+    public static void main(String[] args) {
+        int angka = 1;
+        do {
+            System.out.println("Angka ke-" + angka);
+            angka++;
+        } while (angka <= 5);
+    }
+}
