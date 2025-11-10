76ee8cba95e8c31b576378b9fac5fd0b140e13ab
YufliMentaruk <yuflimentaruk@gmail.com>
2025-11-10 18:56:54 +0800

    Day64.java

---
+++ Day64.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day64 {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai M :");
+        int M = ip.nextInt();
+        System.out.print("Masukkan nilai Pangkat N :");
+        int N = ip.nextInt();
+
+        int hasil = 1;
+        for (int i = 1; i <= N; i++) {
+            hasil *= M;
+        }
+        System.out.println("Hasil dari " + M + " pangkat " + N + " adalah: " + hasil);
+    }
+}
