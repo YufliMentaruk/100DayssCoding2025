21ef7b0127a8f09c2099fefe7968876df6de78a7
YufliMentaruk <yuflimentaruk@gmail.com>
2025-11-26 22:47:05 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,11 @@
+public class Day80 {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+      
+        // Untuk Menampilkan isi array
+        System.out.println("Isi Array:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Index " + i + " = " + a[i]);
+        }
+    }
+}
