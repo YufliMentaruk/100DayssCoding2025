b01af428be95762cad5b35467fbc496f029359a0
YufliMentaruk <yuflimentaruk@gmail.com>
2025-11-27 21:44:53 +0800

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,7 @@
+public class Day81 {
+    public static void main(String[] args) {
+        String[] buah = {"Apel", "Jeruk", "Mangga", "Pisang"};
+        
+        System.out.println(buah[3]);
+    }
+}
