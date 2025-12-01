141d6cb1672f5d4b1a4b24baff09c0fbd644d490
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-01 21:31:12 +0800

    Day85.java

---
+++ Day85.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Day85 {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        // Membuat Ukuran Array
+        System.out.print("Masukkan jumlah elemen array: ");
+        int n = ip.nextInt();
+        int[] angka = new int[n];
+        // Mengisi array
+        for (int i = 0; i < angka.length; i++) {
+            System.out.print("Masukkan angka ke-" + (i + 1) + ": ");
+            angka[i] = ip.nextInt();
+        }
+        // Mencari angka maksimal
+        int max = angka[0];
+        for (int i = 1; i < angka.length; i++) {
+            if (angka[i] > max) {
+                max = angka[i];
+            }
+        }
+        System.out.println("Angka terbesar dalam array adalah: " + max);
+    }
+}
