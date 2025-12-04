9ba0676d87fe6ad9067662ec88f0d4f92a7233f5
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-04 21:17:26 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,10 @@
+public class Day88 {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        // Menggunakan for-each
+        System.out.println("Isi elemen array :");
+        for (int b : a) {
+            System.out.println(b);
+        }
+    }
+}
