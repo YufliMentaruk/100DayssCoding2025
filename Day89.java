484e83c0af3c766595f4570da0ee2994339a9238
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-05 21:30:04 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,10 @@
+public class Day89 {
+    // Method void
+    static void sayHello() {
+        System.out.println("Hallo, Saya Yufli");
+    }
+    public static void main(String[] args) {
+    // Memanggil method void
+        sayHello();
+    }
+}
