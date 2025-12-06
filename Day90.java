81332f4d71893fa2539b88923dbd449af42313e3
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-06 21:57:15 +0800

    Day90.java

--- Day90.java
+++ Day90.java
@@ -1,12 +1,12 @@
 public class Day90 {
 
     // Method non-void (mengembalikan nilai)
-    static int tambah(int a, int b) {
+    static int nilai(int a, int b) {
         return a + b; // mengembalikan hasil penjumlahan
     }
     // Memanggil method dan menyimpan nilai kembaliannya
     public static void main(String[] args) {
-        int hasil = tambah(5, 7); 
+        int hasil = nilai(5, 7); 
         System.out.println("Hasil penjumlahan adalah: " + hasil);
     }
 }
