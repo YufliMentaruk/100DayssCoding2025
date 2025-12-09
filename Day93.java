997da97774e6e27f3981e0eefd4591ffae80f78e
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-09 21:49:42 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,17 @@
+public class Day93 {
+    public static void main(String[] args) {
+
+        double a =4.5;
+        
+        // Pembulatan Keatas
+        System.out.println("=== Contoh Math.ceil() ===");
+        System.out.println("ceil(4.5) = " + Math.ceil(a));
+        // Pembulatan Kebawah
+        System.out.println("\n=== Contoh Math.floor() ===");
+        System.out.println("floor(4.5) = " + Math.floor(a));
+        // Pembulatan Terdekat
+        System.out.println("\n=== Contoh Math.round() ===");
+        System.out.println("round(4.5) = " + Math.round(a));
+
+    }
+}
