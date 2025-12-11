3d50a70398df198f980f1ce9f3818c4f6b8af817
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-11 22:06:43 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,11 @@
+public class Day95 {
+   static void cetaknama (String nama ){
+    // Method void dengan parameter
+     System.out.println("Hallo Nama Saya "+ nama);
+   }
+    // Memanggil method dengan memberikan parameter
+    public static void main(String[] args) {
+        cetaknama("Yufli");
+        
+    }
+}
