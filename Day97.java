c83cf7c48e3a8bd8c1b8a878d42a7557838fd66a
YufliMentaruk <yuflimentaruk@gmail.com>
2025-12-13 22:13:10 +0800

    Day97.java

---
+++ Day97.java
@@ -0,0 +1,8 @@
+public class Day97 {
+    static int luaspersegi(int a){
+        return a*a;
+    }
+    public static void main(String[] args) {
+        System.out.println(luaspersegi(5));
+    }
+}
